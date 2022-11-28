e96685d8df4006842505db54fa7b46af3e4f8477
Emilio Pizzuto <emiliopizzuto99@gmail.com>
2022-11-29 00:03:23 +0100

    - Added Shimmer to RecipesFragment.kt - Created RecyclerView Row with ConstraintLayout

--- app/src/main/java/com/pizzu/pizzufood/RecipesFragment.kt
+++ app/src/main/java/com/pizzu/pizzufood/RecipesFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.facebook.shimmer.ShimmerFrameLayout
 
 
 class RecipesFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        val shimmerFrameLayout = view.findViewById<ShimmerFrameLayout>(R.id.shimmerLayout)
+        shimmerFrameLayout.startShimmer()
+        return view
     }
 }
